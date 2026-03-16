5104b1c1beb54cb08c9b52dfa0064f85775d8fd7
GITVENGAT <vengatprasath2802@gmail.com>
2026-03-16 15:07:37 +0530

    my docker image code

--- app.py
+++ app.py
@@ -1,41 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <html>
-    <head>
-        <title>F1 Ticket Booking</title>
-        <style>
-            body{
-                background-color:#0b0b0b;
-                color:white;
-                text-align:center;
-                font-family:Arial;
-            }
-            h1{
-                color:red;
-                margin-top:80px;
-            }
-            .btn{
-                padding:15px 30px;
-                background:red;
-                color:white;
-                border:none;
-                font-size:18px;
-                cursor:pointer;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Formula 1 Grand Prix Tickets</h1>
-        <p>Book tickets for the fastest racing event on earth</p>
-        <button class="btn">Book Now</button>
-    </body>
-    </html>
-    """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
